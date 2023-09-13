cb31c9fb90652e212722059a5c8d3f1b56c731bb
txema <icetxema@gmail.com>
2023-09-13 22:05:56 +0200

    Second commit

--- sql-6.sql
+++ sql-6.sql
@@ -3,8 +3,8 @@
 USE university_schema;
 
 SELECT s.students_firstName, s.students_lastName,
-	   COUNT(c.courses_id) AS CommonCoursesCount,
-       p.professors_firstName, p.professors_lastName
+	COUNT(c.courses_id) AS CommonCoursesCount,
+	p.professors_firstName, p.professors_lastName
 FROM students s
 JOIN grades g ON s.students_id = g.grades_students_id
 JOIN courses c ON g.grades_courses_id = c.courses_id
